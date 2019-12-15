5e0b1ae05bebf8d9d545d7c20721f149c4107274
aszucsak <aszucsak@gmail.com>
2019-12-15 22:23:13 +0100

    Updates

---
+++ precache-manifest.2daf884bd4097aade62d5671540125fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804ca39765b529507c12c006c5b45654",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b4947efbb721942233ec",
+    "url": "/recipe-app/static/css/main.551a2ea3.chunk.css"
+  },
+  {
+    "revision": "170a0909e2824c518ef0",
+    "url": "/recipe-app/static/js/2.3aec082f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.3aec082f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4947efbb721942233ec",
+    "url": "/recipe-app/static/js/main.aa56ffbb.chunk.js"
+  },
+  {
+    "revision": "c9e78abdeae374382543",
+    "url": "/recipe-app/static/js/runtime-main.c7667b32.js"
+  }
+]);
